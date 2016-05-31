4417d809059885ba5d5524de93db22c3a95b5aa1
Yuriy <Navuchodonosor@users.noreply.github.com>
2016-05-31 15:55:23 +0300

    Change some functions
    added chek in retrieving, added printing and txt file sender.

--- DifferentCode/dbase.py
+++ DifferentCode/dbase.py
@@ -20,7 +20,7 @@ class Dbase(object):
             self.cur.execute('SELECT id FROM Username WHERE username = ?',(username,))
             test=self.cur.fetchone()
             if test is not None:
-                username_id=self.cur.fetchone()[0]
+                username_id=test[0]
                 self.cur.execute('''UPDATE Password SET password = ? WHERE username_id = ?''', (password, username_id))
                 self.conn.commit()
         else:
@@ -30,17 +30,27 @@ class Dbase(object):
             self.conn.commit()
     
     def addNameAndPassword(self, username, password, replace = True):
+        self.cur.execute('''INSERT OR IGNORE INTO Username (username) VALUES (?)''', (username,))
+        self.conn.commit()
         if replace == True:
-            self.cur.execute('''INSERT OR IGNORE INTO Username (username) VALUES (?)''', (username,))
-            self.conn.commit()
+            self.cur.execute('SELECT id FROM Username WHERE username = ?',(username,))
+            test=self.cur.fetchone()
+            if test is not None:
+                username_id=test[0]
+                self.cur.execute('SELECT username_id FROM Password WHERE username_id = ?', (username_id,))
+                item = self.cur.fetchone()
+                if item is not None:
+                    self.cur.execute('''UPDATE Password SET password = ? WHERE username_id = ?''', (password, username_id))
+                    self.conn.commit()
+                else:
+                    self.cur.execute('''INSERT OR REPLACE INTO Password (username_id, password) VALUES (?,?)''', (username_id, password))
+                    self.conn.commit()
+        else:
             self.cur.execute('SELECT id FROM Username WHERE username = ?',(username,))
             username_id=self.cur.fetchone()[0]
-            if self.cur.execute('') != None:
-                self.cur.execute('''INSERT OR REPLACE INTO Password (username_id, password) VALUES (?,?)''', (username_id, password))
-                self.conn.commit()
-            else:
-                self.cur.execute('''UPDATE Password SET password = ? WHERE username_id = ?''', (password, username_id))
-                self.conn.commit()
+            self.cur.execute('''INSERT OR REPLACE INTO Password (username_id, password) VALUES (?,?)''', (username_id, password))
+            self.conn.commit()
+            
             
     def chekUserName(self, username):
         self.cur.execute('SELECT Username.id, Username.username FROM Username WHERE username = ?', (username,))
@@ -61,35 +71,72 @@ class Dbase(object):
         else:
             print 'This user has no password or there is no such user in database.'
     
-    def retrieveUsers(self):
-        selected = self.cur.execute('SELECT * FROM Username')
-        for item in selected:
-            print item
-    
-    def retrievePasswords(self):
-        selected = self.cur.execute('SELECT * FROM Password')
-        for item in selected:
-            print item
-    
-    def retrieveAll(self):
-        self.cur.execute('SELECT Username.username,Password.password FROM Username INNER JOIN Password ON Username.id = Password.username_id ORDER BY Username.id')
-        selected = self.cur.fetchone()
-        if selected is not None:
-            print selected
+    def retrieveUsers(self, prn=True):
+        self.cur.execute('SELECT * FROM Username')
+        selected = self.cur.fetchall()
+        if not selected:
+            return 'No entries found.'
+        elif selected is not None and prn == True:
+            for id_,username in selected:
+                print 'id: '+str(id_)+' Username: '+str(username)
+        elif selected is not None and prn == False:
+            return selected
         else:
-            print 'Your database has no enteries.'
+            return None
     
+    def retrievePasswords(self, prn=True):
+        self.cur.execute('SELECT * FROM Password')
+        selected = self.cur.fetchall()
+        if not selected:
+            return 'No entries found.'
+        elif selected is not None and prn == True:
+            for id_,password in selected:
+                print 'id: '+str(id_)+' password: '+str(password)
+        elif selected is not None and prn == False:
+            return selected
+    
+    def retrieveAll(self, prn=True):
+        self.cur.execute('SELECT Username.username, Password.password FROM Username INNER JOIN Password ON Username.id = Password.username_id')
+        selected = self.cur.fetchall()
+        if not selected:
+            if self.retrieveUsers(False) != None and prn == True:
+                return self.retrieveUsers()
+            elif self.retrieveUsers(False) != None and prn == False:
+                return self.retrieveUsers(False)
+            else:
+                return 'You have users without passwords.'
+        elif selected is not None and prn == True:
+            for name,password in selected:
+                print 'Username: '+str(name)+', password: '+str(password)
+        elif selected is not None and prn == False:
+            return selected
+        else:
+            return None
+        
     def deleteUser(self, username):
         self.cur.execute('SELECT id FROM Username WHERE username = ?',(username,))
         test=self.cur.fetchone()
         if test is not None:
-            username_id=self.cur.fetchone()[0]
+            username_id=test[0]
             self.cur.execute('DELETE FROM Username WHERE username = ?',(username,))
             self.conn.commit()
             self.cur.execute('DELETE FROM Password WHERE username_id = ?',(username_id,))
             self.conn.commit()
         else:
             print 'No such user in database.'
+        
+    def cleanDatabase(self):
+        self.cur.executescript('''
+        DELETE FROM Username; 
+        DELETE FROM Password;
+        VACUUM;''')
+        self.conn.commit()
+        
+    #def makeCSV(self,filename):
+        
+    def makeTxt(self, filename):
+        with open(filename, 'w') as f_out:
+            f_out.write(str(self.retrieveAll(False)))
     
     def closeConnection(self):
         self.conn.commit()
